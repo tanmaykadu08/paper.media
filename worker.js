bd0963e78cb478b6cf5dd65672a2b49766e25af9
Tanmay Kadu <kadutanmay.06@gmail.com>
2026-05-01 02:23:28 +0530

    remove ther error

--- worker.js
+++ worker.js
@@ -1,27 +1,30 @@
 export default {
-  async fetch(request) {
-    const url = new URL(request.url);
+async fetch(request) {
+const url = new URL(request.url);
 
-    if (request.method === "POST" && url.pathname === "/contact") {
-      try {
-        const data = await request.json();
 
-        return new Response(
-          JSON.stringify({
-            success: true,
-            message: "Received",
-            data
-          }),
-          { headers: { "Content-Type": "application/json" } }
-        );
-      } catch {
-        return new Response(
-          JSON.stringify({ success: false, error: "Invalid JSON" }),
-          { status: 400 }
-        );
-      }
-    }
+if (request.method === "POST" && url.pathname === "/contact") {
+  try {
+    const data = await request.json();
 
-    return new Response("API is working 🚀");
+    return new Response(JSON.stringify({
+      success: true,
+      message: "Received",
+      data
+    }), {
+      headers: { "Content-Type": "application/json" }
+    });
+
+  } catch (error) {
+    return new Response(JSON.stringify({
+      success: false,
+      error: "Invalid JSON"
+    }), { status: 400 });
   }
+}
+
+return new Response("API is working 🚀");
+
+
+}
 };
